22bb5fb9fa1191c6a1776f10350a694e3a31e11b
maahi65f <144053424+maahi6@users.noreply.github.com>
2023-09-04 11:22:57 -0700

    Add files via upload

---
+++ psuedocode1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+// Headers
+string toString (double);
+int toInt (string);
+double toDouble (string);
+
+int main() {
+    int x, y, sum, average;
+
+    cin >> x;
+    cin >> y;
+    sum = x + y;
+    average = (int) ((double) sum / 2);
+    cout << average << endl;
+    return 0;
+}
+
+// The following implements type conversion functions.
+string toString (double value) { //int also
+    stringstream temp;
+    temp << value;
+    return temp.str();
+}
+
+int toInt (string text) {
+    return atoi(text.c_str());
+}
+
+double toDouble (string text) {
+    return atof(text.c_str());
+}
